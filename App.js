b82beae79e0b113a9d3bb06eeff9636b20e230af
Nidhi Modi <modini.tandg@gmail.com>
2020-09-29 14:23:30 +1300

    first commit from ios

---
+++ App.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import SplashScreen from 'react-native-splash-screen'
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    
+  }
+
+
+
+  async componentDidMount() {
+
+    SplashScreen.hide();
+
+  }
+
+  render() {
+    return (
+
+
+    <View>
+
+      <Text style={styles.welcome}>WELCOME TO REACT NATIVE</Text>
+
+    </View>
+
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+});
